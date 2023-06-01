c9f16e92310a97bf2ce0a9b7b0113d3358d7b89f
IsaacRFx <edwin.rodriguez193@tectijuana.edu.mx>
2023-05-31 21:21:01 -0700

    New Learning Test
    Implement new learning type test:)

--- api/serializers/test_metrics.py
+++ api/serializers/test_metrics.py
@@ -1,7 +1,8 @@
 from django.contrib.auth.models import UserManager
 from rest_framework import serializers
 
-from api.models import TechUser, KuderTest
+from api.models import KuderTest, LearningTypeTest, TechUser
+
 
 class KuderTestSerializer(serializers.ModelSerializer):
     result_pretty = serializers.SerializerMethodField(read_only=True) 
@@ -13,6 +14,15 @@ class KuderTestSerializer(serializers.ModelSerializer):
     def get_result_pretty(self, obj):
         return obj.get_result_display()
 
+class LearningTypeSerializer(serializers.ModelSerializer):
+    result_pretty = serializers.SerializerMethodField(read_only=True) 
+
+    class Meta:
+        model = LearningTypeTest
+        fields = '__all__'
+
+    def get_result_pretty(self, obj):
+        return obj.get_result_display()
 
 class TestSerializer(serializers.ModelSerializer):
 
--- api/views/test_metrics.py
+++ api/views/test_metrics.py
@@ -3,9 +3,11 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from api.models import KuderTest, TechUser
-from api.serializers.test_metrics import KuderTestSerializer
+from api.models import KuderTest, LearningTypeTest, TechUser
+from api.serializers.test_metrics import (KuderTestSerializer,
+                                          LearningTypeSerializer)
 from api.serializers.user import UserSignUpSerializer
+
 
 def validate_user(request, user_id):
     try:
@@ -16,6 +18,7 @@ def validate_user(request, user_id):
     # if not request.user.id == user.id:
     #     return False
     return user
+
 
 class KuderView(APIView):
     def get(self, request, user_id):
@@ -62,3 +65,81 @@ class KuderView(APIView):
             )
         except Exception as e:
             return Response({"error": str(e)}, status.HTTP_400_BAD_REQUEST)
+
+
+class LearningTypeView(APIView):
+    def get(self, request, user_id):
+        user = validate_user(request, user_id)
+
+        if user is False:
+            return Response(
+                {"error": "You are not authorized to perform this action"},
+                status.HTTP_401_UNAUTHORIZED,
+            )
+        try:
+            learning = LearningTypeTest.objects.filter(user=user).last()
+            if learning is None:
+                return Response(
+                    {"message": "No learning test found"}, status.HTTP_404_NOT_FOUND
+                )
+            serializer = LearningTypeSerializer(learning)
+            return Response({"message": serializer.data}, status.HTTP_200_OK)
+        except Exception as e:
+            return Response({"error": str(e)}, status.HTTP_404_NOT_FOUND)
+
+    def post(self, request, user_id):
+        data = request.data
+        user = validate_user(request, user_id)
+
+        if user is False:
+            return Response(
+                {"error": "You are not authorized to perform this action"},
+                status.HTTP_401_UNAUTHORIZED,
+            )
+
+        try:
+            item = LearningTypeTest.objects.create(
+                user=user,
+                result=data["result"],
+            )
+            item.save()
+            return Response(
+                {
+                    "message": "LearningType test saved",
+                    "test": {"id": item.id, "result": item.result},
+                },
+                status.HTTP_201_CREATED,
+            )
+        except Exception as e:
+            return Response({"error": str(e)}, status.HTTP_400_BAD_REQUEST)
+
+
+class AllTestView(APIView):
+    def get(self, request, user_id):
+        user = validate_user(request, user_id)
+
+        if user is False:
+            return Response(
+                {"error": "You are not authorized to perform this action"},
+                status.HTTP_401_UNAUTHORIZED,
+            )
+        try:
+            kuder = KuderTest.objects.filter(user=user).last()
+            learning = LearningTypeTest.objects.filter(user=user).last()
+            kuder_data = (
+                KuderTestSerializer(kuder).data
+                if kuder is not None
+                else "No entries found"
+            )
+
+            learning_data = (
+                LearningTypeSerializer(learning).data
+                if learning is not None
+                else "No entries found"
+            )
+            return Response(
+                {"tests": {"kuder": kuder_data, "learning": learning_data}},
+                status.HTTP_200_OK,
+            )
+        except Exception as e:
+            return Response({"error": str(e)}, status.HTTP_404_NOT_FOUND)
